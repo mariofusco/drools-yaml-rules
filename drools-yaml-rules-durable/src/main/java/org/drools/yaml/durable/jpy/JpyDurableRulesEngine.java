d94c1240afd84945bfad06d1170f84f906b173d5
Edoardo Vacchi <evacchi@users.noreply.github.com>
2022-08-18 18:28:30 +0200

    Add JPY "official" public API (#7)

---
+++ drools-yaml-rules-durable/src/main/java/org/drools/yaml/durable/jpy/JpyDurableRulesEngine.java
@@ -0,0 +1,72 @@
+package org.drools.yaml.durable.jpy;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.drools.yaml.core.RulesExecutor;
+import org.drools.yaml.core.RulesExecutorContainer;
+import org.drools.yaml.durable.DurableNotation;
+import org.drools.yaml.durable.domain.DurableRuleMatch;
+import org.kie.api.runtime.rule.Match;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import static org.drools.yaml.core.RulesExecutor.OBJECT_MAPPER;
+
+public class JpyDurableRulesEngine {
+
+    private Iterator<Map<String, Map>> lastResponse = Collections.emptyIterator();
+
+    public long createRuleset(String ruleSetName, String rulesetString) {
+        RulesExecutor executor = RulesExecutor.createFromJson(
+                DurableNotation.INSTANCE,
+                String.format("{\"%s\":%s}", ruleSetName, rulesetString));
+        return executor.getId();
+    }
+
+    /**
+     * @return error code (currently always 0)
+     */
+    public int retractFact(long sessionId, String serializedFact) {
+        RulesExecutorContainer.INSTANCE.get(sessionId).retract(serializedFact);
+        return 0;
+    }
+
+    public String advanceState() {
+        if (lastResponse.hasNext()) {
+            Map<String, Map> elem = lastResponse.next();
+            try {
+                return OBJECT_MAPPER.writeValueAsString(elem);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public int assertFact(long sessionId, String serializedFact) {
+        return processMessage(
+                serializedFact,
+                RulesExecutorContainer.INSTANCE.get(sessionId)::processFacts);
+    }
+
+    public int assertEvent(long sessionId, String serializedFact) {
+        return processMessage(
+                serializedFact,
+                RulesExecutorContainer.INSTANCE.get(sessionId)::processEvents);
+    }
+
+    public String getFacts(long session_id) {
+        return RulesExecutorContainer.INSTANCE.get(session_id).getAllFactsAsJson();
+    }
+
+    private int processMessage(String serializedFact, Function<String, Collection<Match>> command) {
+        List<Map<String, Map>> lastResponse = DurableRuleMatch.asList(command.apply(serializedFact));
+        this.lastResponse = lastResponse.iterator();
+        return 0;
+    }
+}
