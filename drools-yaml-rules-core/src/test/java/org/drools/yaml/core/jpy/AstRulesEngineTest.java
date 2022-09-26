81ebfecee9f931142059fc37d636d66e0de8422a
Edoardo Vacchi <evacchi@users.noreply.github.com>
2022-09-26 16:39:58 +0200

    AstRulesEngine: Return List (#13)

---
+++ drools-yaml-rules-core/src/test/java/org/drools/yaml/core/jpy/AstRulesEngineTest.java
@@ -0,0 +1,23 @@
+package org.drools.yaml.core.jpy;
+
+import org.drools.yaml.core.JsonTest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+
+public class AstRulesEngineTest {
+    @Test
+    public void testJpyApi() {
+
+        String rules = JsonTest.JSON1;
+
+        AstRulesEngine engine = new AstRulesEngine();
+        long id = engine.createRuleset(rules);
+
+        String result = engine.assertFact(id, "{ \"sensu\": { \"data\": { \"i\":1 } } }");
+
+        assertNotNull(result);
+    }
+}
